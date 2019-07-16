1f057e18ec6f0fd0910b1234e8c216652381ccd6
Haibin Zhu <ssspiderzzz@gmail.com>
2019-07-16 15:11:33 -0700

    add counter js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('#inputText').on('input propertychange', function() {
+    let counter = $("#inputText").val().trim().length;
+    $('.counter').html(`${140 - counter}`)
+    if (counter > 140) {
+      $('.counter').css('color', 'red')
+    }
+  })
+
+});
