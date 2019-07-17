ae31b52e92866865ddcf922e247959598f78a730
ssspiderzzz <ssspiderzzz@gmail.com>
2019-07-17 22:24:42 +0000

    d3 ajax get and post, error handler, minor css updates

---
+++ public/scripts/animation.js
@@ -0,0 +1,15 @@
+$(() => {
+
+// Animation for the arrow down
+  function arrows() {
+    let a;
+    a = $('#arrow')
+    a.css('top', '0px');
+    setTimeout(function () {
+      a.css('top', '15px');;
+    }, 1000);
+  }
+  arrows();
+  setInterval(arrows, 2000);
+
+});
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,34 @@
 $(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+
+const errorTextInput = function(counter) {
+  $('.errorBox').empty();
+  let conditionalText = "";
+  if (counter > 140) {
+    conditionalText = `Too long! Please respect our arbitrary limit of 140 chars. #kthxbye`
+  } else if (counter === 0 || counter === null) {
+    conditionalText = `Please type something! Empty tweets are not allowed. #kthxbye`
   }
-]
+  let $error = $('<div>').addClass('errorBox')
+  $error.html(`
+    <article class="error">
+      <div class="errorText">
+        <img id="danger1" src="https://img.icons8.com/clouds/100/000000/error.png">
+          ${conditionalText}
+        <img id="danger2" src="https://img.icons8.com/clouds/100/000000/error.png">
+       </div>
+    </article>
+  `)
+  $('.container').prepend($error);
+}
 
 const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
-for (let i of tweets) {
-  const tweet = createTweetElement(i);
-  $('.tweets').append(tweet);
-}
+  for (let i of tweets) {
+    const tweet = createTweetElement(i);
+    $('.tweets').prepend(tweet);
+  }
 }
 
 const createTweetElement = function(tweet) {
@@ -51,11 +48,28 @@ $tweet.html(`
       <img id="refresh" height="30" width="30" src="https://img.icons8.com/officexs/16/000000/repeat.png"></img>
       <img id="like" height="30" width="30" src="https://img.icons8.com/ultraviolet/40/000000/like.png"></img>
     </footer>
-
 `)
 return $tweet;
 }
 
-renderTweets(data);
+$.ajax('/tweets', { method: 'GET' }).then(renderTweets);
+
+const $submit = $('form');
+$submit.on('submit',(event) => {
+  console.log('Button clicked, performing ajax call...');
+  event.preventDefault();
+  // read the text counter
+  let counter = $("#inputText").val().trim().length;
+  if (counter > 140 || counter === 0 || counter === null) {
+    errorTextInput(counter);
+    return false;
+  }
+  //
+  $.post('/tweets', $submit.serialize(), () => {
+    $('.errorBox').empty();
+    $.ajax('/tweets', { method: 'GET' }).then(renderTweets);
+  })
+});
+
 
 });
