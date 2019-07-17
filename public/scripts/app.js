8fb5da0c532dce071f4f45c0138d9f4dde91b528
Haibin Zhu <spiderz@Haibins-MacBook-Air.local>
2019-07-16 19:48:05 -0700

    d2 done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Fake data taken from initial-tweets.json
+$(document).ready(function() {
 const data = [
   {
     "user": {
@@ -34,12 +28,34 @@ const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
+for (let i of tweets) {
+  const tweet = createTweetElement(i);
+  $('.tweets').append(tweet);
+}
 }
 
 const createTweetElement = function(tweet) {
 let $tweet = $('<article>').addClass('tweet');
-// ...
+$tweet.html(`
+    <div class="avatarNameHandle">
+            <img class="avatar" src="${tweet.user.avatars}">
+            <span class="userName">${tweet.user.name}</span>
+            <span class="userHandle">${tweet.user.handle}</span>
+    </div>
+    <hr />
+      <div class="tweetContent">${tweet.content.text}</div>
+      <div class="border"></div>
+      <footer class="tweetTime">${new Date(tweet.created_at)}</footer>
+    <footer class="tweetIcon">
+      <img id="flag" height="30" width="30" src="https://img.icons8.com/officel/16/000000/flag.png"></img>
+      <img id="refresh" height="30" width="30" src="https://img.icons8.com/officexs/16/000000/repeat.png"></img>
+      <img id="like" height="30" width="30" src="https://img.icons8.com/ultraviolet/40/000000/like.png"></img>
+    </footer>
+
+`)
 return $tweet;
 }
 
 renderTweets(data);
+
+});
