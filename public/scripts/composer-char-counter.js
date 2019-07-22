e4958b3816c2ede0e44ed73d5626386764a55f6a
ssspiderzzz <ssspiderzzz@gmail.com>
2019-07-22 20:45:24 +0000

    fixed the problem of not counting empty spaces, fixed css for not showing empty spaces in tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $('#inputText').on('input propertychange', function() {
-    let counter = $("#inputText").val().trim().length;
+    let counter = $("#inputText").val().length;
     $('.counter').html(`${140 - counter}`)
     if (counter > 140) {
       $('.counter').css('color', 'red')
